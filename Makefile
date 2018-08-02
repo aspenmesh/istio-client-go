00e04f6885f4b696033da463b0893626852ee8b2
Neeraj Poddar <neeraj@aspenmesh.io>
2018-08-01 18:36:31 -0600

    Added Istio Authentication Policy resource

--- Makefile
+++ Makefile
@@ -10,14 +10,14 @@ endif
 # pkg/client is auto generated code
 # zz_generated.deepcopy.go under pkg/apis/<group>/<version>/ is also generated
 # code
-DIRS := pkg/apis/networking pkg/apis/networking/v1alpha3
+DIRS := pkg/apis/networking pkg/apis/networking/v1alpha3 pkg/apis/authentication pkg/apis/authentication/v1alpha1
 DEPS_ALL := $(foreach dir, $(DIRS), $(wildcard $(dir)/*.go))
 GENERATED_FILES_PATTERN := %zz_generated.deepcopy.go
 DEPS := $(filter-out $(GENERATED_FILES_PATTERN), $(DEPS_ALL))
 GENERATED_FILES := $(filter $(GENERATED_FILES_PATTERN), $(DEPS_ALL))
 BOILERPLATE := aspenmesh-boilerplate.go.txt
 
-GROUP_VERSIONS := "networking:v1alpha3"
+GROUP_VERSIONS := "networking:v1alpha3, authentication:v1alpha1"
 
 all: generate-code test
 
---
+++ pkg/apis/authentication/v1alpha1/policy.go
@@ -0,0 +1,53 @@
+/*
+Portions Copyright 2017 The Kubernetes Authors.
+Portions Copyright 2018 Istio Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	istiov1alpha1 "istio.io/api/authentication/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Policy is a Istio Policy resource
+type Policy struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec PolicySpec `json:"spec"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PolicyList is a list of Policy resources
+type PolicyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Policy `json:"items"`
+}
+
+// PolicySpec in a wrapper around Istio Policy
+type PolicySpec struct {
+	istiov1alpha1.Policy
+}
+
+// DeepCopyInto is a deepcopy function, copying the receiver, writing into out. in must be non-nil.
+// Based of https://github.com/istio/istio/blob/release-0.8/pilot/pkg/config/kube/crd/types.go#L450
+func (in *PolicySpec) DeepCopyInto(out *PolicySpec) {
+	*out = *in
+}

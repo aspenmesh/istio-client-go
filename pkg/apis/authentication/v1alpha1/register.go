82110b9a99ba9b93943df81bfeccc764072a3f42
Madison Eaton <madison127@gmail.com>
2018-09-05 11:18:56 -0600

    Added Istio MeshPolicy resource

--- pkg/apis/authentication/v1alpha1/register.go
+++ pkg/apis/authentication/v1alpha1/register.go
@@ -46,6 +46,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&Policy{},
 		&PolicyList{},
+		&MeshPolicy{},
+		&MeshPolicyList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil

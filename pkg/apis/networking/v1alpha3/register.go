3cebc13240d6b5f892877879b65eca2103f5d5d2
Neeraj Poddar <neeraj@aspenmesh.io>
2019-04-17 11:39:25 -0600

    Added APIs for Gateway resource

--- pkg/apis/networking/v1alpha3/register.go
+++ pkg/apis/networking/v1alpha3/register.go
@@ -48,6 +48,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&VirtualServiceList{},
 		&DestinationRule{},
 		&DestinationRuleList{},
+		&Gateway{},
+		&GatewayList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil

db2df73903f0dc4141c010c360485bada1f5d5ca
Steven Dake <steven.dake@netapp.com>
2019-04-25 14:27:37 -0700

    Add EnvoyFilter support to API
    1 of 3 patches to add EnvoyFilter support to the api.

--- pkg/apis/networking/v1alpha3/register.go
+++ pkg/apis/networking/v1alpha3/register.go
@@ -52,6 +52,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&GatewayList{},
 		&ServiceEntry{},
 		&ServiceEntryList{},
+		&EnvoyFilter{},
+		&EnvoyFilterList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
